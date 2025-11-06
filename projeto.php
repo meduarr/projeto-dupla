efb86a8732d5eb5644c258a452d575f9a8aaa494
meduarr <me972863@gmail.com>
2025-11-06 19:26:09 -0300

    ajustes no codigo

--- projeto.php
+++ projeto.php
@@ -9,18 +9,23 @@
 <body>
 
     <?php
-    echo '<form>';
-       echo '<fieldset>';
-        echo '<h2> Olá </h2>';
-          echo 'Username: <input type="text" name="Usuário" required> <br> <br>';
-          echo 'Password: <input type="password" name="Senha" required> <br> <br>';
-        echo '<button type="submit"> Entrar </button>';
-          echo '<h3> Não tem conta?</h3>';
-          echo '<a href " "> Cadastre-se</a>';
-       echo '</fieldset>';
+
+    $titulo_form = "Olá,";
+    $texto_cadastro = "Não tem conta?";
+    $url_cadastro = "cadastro.php";
+
+     echo '<form method="POST action=" ">';
+     echo '<fieldset>';
+     echo '<h2> '. $titulo_form .' </h2>';
+     echo 'Username: <input type="text" name="Usuário" required> <br> <br>';
+     echo 'Password: <input type="password" name="Senha" required> <br> <br>';
+     echo '<button type="submit"> Entrar </button>';
+     echo '<h3> '. $texto_cadastro .'</h3>';
+     echo '<a href " "> '. $url_cadastro.' </a>';
+     echo '</fieldset>';
     echo '</form>';
 
     ?>
-    
+
 </body>
 </html>
